ca03527cabc58687e25160cbbab4f5851215fbe7
Danny Shan <dani_davy@hotmail.com>
2018-11-03 10:27:47 +0800

    use geth instead

--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -27,7 +27,8 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "4",
-      from: "0xA60895898320599e2F1bfffB3BB155879091448c"
+      from: "0xA60895898320599e2F1bfffB3BB155879091448c",
+      gas: 4500000
     },
     rinkeby: {
       provider: function() { 
